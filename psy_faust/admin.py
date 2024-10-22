16ed63d9144c36361a41249bc7b4fef277896d88
Igor <104066443+OldXrist@users.noreply.github.com>
2024-10-22 10:21:45 +0300

    register contacts in admin

--- psy_faust/admin.py
+++ psy_faust/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Qualifications, FAQ
+from .models import Qualifications, FAQ, Contacts
 
 # Register your models here.
 
 admin.site.register(Qualifications)
 admin.site.register(FAQ)
+admin.site.register(Contacts)
