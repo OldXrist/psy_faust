7d3d72d4f3c07fa0d830df4a456edd2de96b1be9
Igor <104066443+OldXrist@users.noreply.github.com>
2024-10-22 10:37:16 +0300

    Update views.py

--- psy_faust/views.py
+++ psy_faust/views.py
@@ -5,5 +5,5 @@ from .models import Qualifications, FAQ, Contacts
 def index(request):
     qualifications = Qualifications.objects.all()
     faq = FAQ.objects.all()
-    contacts = Contacts.objects.first()
+    contacts = Contacts.objects.all().first()
     return render(request, 'index.html', {'qualifications': qualifications, 'FAQ': faq, 'contacts': contacts})
