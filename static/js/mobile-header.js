dfc00d4188b89e85d9732826272ce3d122069dd3
Igor <104066443+OldXrist@users.noreply.github.com>
2025-01-11 18:10:27 +0300

    Logo in mobile + update breakpoints

--- static/js/mobile-header.js
+++ static/js/mobile-header.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('.header-mb').hide()
-    $('.header-control').click(function () {
+    $('.header-control svg').click(function () {
         $('.header-mb').slideToggle()
         if (  $( this ).css( "transform" ) == 'none' ){
             $(this).css("transform","rotate(180deg)");
