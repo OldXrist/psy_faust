1a5cd0777ae8fc1ceba38ac57c368597bb7e8a10
Igor <104066443+OldXrist@users.noreply.github.com>
2025-01-14 19:03:48 +0300

    Update admin.py

--- psy_faust/admin.py
+++ psy_faust/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Qualifications, FAQ, Contacts
+from .models import Qualifications, FAQ, Contacts, Services
 
 # Register your models here.
 
 admin.site.register(Qualifications)
 admin.site.register(FAQ)
 admin.site.register(Contacts)
+admin.site.register(Services)
