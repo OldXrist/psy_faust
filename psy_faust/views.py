df5f8dd804e712e939c062f70397ac56cc8f0309
Igor <104066443+OldXrist@users.noreply.github.com>
2025-01-14 19:05:50 +0300

    Update views.py

--- psy_faust/views.py
+++ psy_faust/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from .models import Qualifications, FAQ, Contacts
+from .models import Qualifications, FAQ, Contacts, Services
 
 
 def index(request):
     qualifications = Qualifications.objects.all()
     faq = FAQ.objects.all()
     contacts = Contacts.objects.all().first()
-    return render(request, 'index.html', {'qualifications': qualifications, 'FAQ': faq, 'contacts': contacts})
+    services = Services.objects.all()
+    return render(request, 'index.html', {'qualifications': qualifications,
+                                          'FAQ': faq,
+                                          'contacts': contacts,
+                                          'services': services})
